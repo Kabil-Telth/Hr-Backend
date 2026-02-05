dd40a9bef063a3aa20cbefb198b935214f03dd8c
telthhealth-cloud <telthhealth@gmail.com>
2026-02-05 09:59:10 +0530

    xtend accesstoken time

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,7 +4,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user._id, email: user.email },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: '15m' }
+    { expiresIn: '1d' }
   );
 };
 
